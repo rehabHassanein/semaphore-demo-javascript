91e24adbc6de9a93e2b4b35719f6771c11e67cff
$GH_USERNAME <$GH_EMAIL>
2019-04-16 15:17:05 +0000

    Updates

--- precache-manifest.17fb5c14cb5836bd26103f6dd8dbbfeb.js
+++ precache-manifest.17fb5c14cb5836bd26103f6dd8dbbfeb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/semaphore-demo-javascript/static/js/runtime~main.2007b693.js"
   },
   {
-    "revision": "a0dce053bc2f43d682a3",
-    "url": "/semaphore-demo-javascript/static/js/main.a0dce053.chunk.js"
+    "revision": "559b6d2e7e8d3e9358d2",
+    "url": "/semaphore-demo-javascript/static/js/main.559b6d2e.chunk.js"
   },
   {
     "revision": "10ca694364fa0726d4ce",
     "url": "/semaphore-demo-javascript/static/js/1.10ca6943.chunk.js"
   },
   {
-    "revision": "a0dce053bc2f43d682a3",
+    "revision": "559b6d2e7e8d3e9358d2",
     "url": "/semaphore-demo-javascript/static/css/main.056b25b3.chunk.css"
   },
   {
-    "revision": "09a2bf7a1952e79a9e948f685a906e88",
+    "revision": "d3f81f2de54c914c3b57b0d9e104cfd8",
     "url": "/semaphore-demo-javascript/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/semaphore-demo-javascript/precache-manifest.5e1a0ab3b4d64cc1925a0641174d01d2.js"
+  "/semaphore-demo-javascript/precache-manifest.17fb5c14cb5836bd26103f6dd8dbbfeb.js"
 );
 
 workbox.clientsClaim();
