1bcfee9743f54ddb040b2424a5fc7d75493ac294
Rehab Hassanein <rehab.s.hassanein@gmail.com>
2019-05-07 18:37:44 +0300

    Disabling cors

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: false });
   const configService: ConfigService = app.get(ConfigService);
   app.setGlobalPrefix(configService.get('URL_PREFIX'));
   await app.listen(configService.get('PORT'));
