e70896ccba98f2572af96e631c4c02336f4a05b4
Rehab Hassanein <rehab.s.hassanein@gmail.com>
2019-05-08 14:35:45 +0300

    Restoring original state

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService: ConfigService = app.get(ConfigService);
   app.setGlobalPrefix(configService.get('URL_PREFIX'));
   await app.listen(configService.get('PORT'));
